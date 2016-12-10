337fbb66197abf9fce5c3d1bd4b7ae1ef56707ae
KadoBOT <ricardo.ambrogi@gmail.com>
2016-12-10 17:50:33 -0200

    full screen responsiveness

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './container/App';
 import configureStore from './store';
 import './base.css'
 
-
 const initialState = process.env.REACT_APP_ENV === 'template' ? templateState : { } //if template, loads an initial state to the store
 const store = configureStore(initialState)
 
